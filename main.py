5dce68b020fa64d83f539980537f98738da6cba1
Eric Muheto <muheric2012@gmail.com>
2025-05-30 19:35:48 -0400

    removed comments to make the code much readable

--- main.py
+++ main.py
@@ -6,28 +6,18 @@ from stats import presentation,  get_num_words, test
 import sys
 
 def get_book_text():
-    #filepath=os.path.join(os.getcwd(),"books/frankenstein.txt")
-    #if os.path.isfile(filepath):
-    #filepath=sys.argv[1]
-   # if len(sys.argv) < 2:
-    #    print("python3 main.py <path_to_book>")
 
     try:
         if os.path.isfile(sys.argv[1]):
             filepath=sys.argv[1]
             with open(filepath, "r") as f:
                  file_contents=f.read()
-       # elif len(sys.argv) < 2:
-        #    print("Usage: python3 main.py <path_to_book>")
-         #   sys.exit(1)
     except IndexError:
         print ("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
     except ValueError:
         print("Value provided is not a path".format(sys.argv[1]))
         sys.exit(1)
-    #else:
-     #   print("no file found")
     return file_contents
 
 def main():
--- stats.py
+++ stats.py
@@ -10,16 +10,9 @@ def test(lowered_content):
     str1=lowered_content.lower()
     for c in set(str1):
         freq[c]=str1.count(c)
-    #print(freq)
     return freq
 
 def presentation(sort):
-    #print("============ BOOKBOT ============")
-    #print("Analyzing book found at books/frankenstein.txt...")
-    #print("--------- Character Count -------")
-
-   # sorted_dict_descending_order=dict(sorted(sort.items(), key=lambda x: (x[1][1], -x[1][0])))
-    #print(sorted_dict_descending_order)
     attempted_solution=dict(sorted(sort.items(), key=itemgetter(1),reverse=True))
 
 
